296f154f78cac71515117f3166372803f9d17d31
Keith Jeter <kjeter2001@yahoo.com>
2017-12-27 15:21:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 const app = "I don't do much."
+
+var kittens = array["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(array) {
+  return kittens.push("Ralph");
+}
+
+function destructivelyPrependKitten() {
+  
+}
+
+function destructivelyRemoveLastKitten() {
+  
+}
+
+function destructivelyRemoveFirstKitten() {
+  
+}
+
+function appendKitten() {
+  
+}
+
+function prependKitten() {
+  
+}
